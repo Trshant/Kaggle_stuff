44965a239526975effeb3b11944b0218fd905c46
rupsaijna <rupsajina@gmail.com>
2017-02-21 16:52:29 +0530

    added read and mung func

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,13 +4,11 @@ import csv
 import numpy as np
 
 
-def train(filename):
-    df = pd.read_csv(filename)
-    df['Age'].fillna('30.0', inplace=True )
+def train(df):
+   #df['Age'].fillna('30.0', inplace=True )
     featurespace=[]
     survival=[]
     for index, row in df.iterrows():
-        ##print row["PassengerId"]
         fr=[]
         fr.append(int(row["Pclass"]))
         if row["Sex"]=='male':
@@ -28,10 +26,9 @@ def train(filename):
     nb_classifier= gnb.fit(featurespace, survival)
     return nb_classifier
 
-def test(classifier , filename ):
-    df = pd.read_csv(filename)
-    df['Age'].fillna('30.0',inplace=True) 
-    df['Fare'].fillna('35.6', inplace=True ) 
+def test(classifier , df):
+    #df['Age'].fillna('30.0',inplace=True) 
+    #df['Fare'].fillna('35.6', inplace=True ) 
     featurespace=[]
     main=[]
     for index, row in df.iterrows():
@@ -51,7 +48,25 @@ def test(classifier , filename ):
     res=zip(main,results)
     return res
 
-t = train('./files/train.csv')
-tr = test(t,'./files/test.csv')
+def read_CSV(filename):
+	df = pd.read_csv(filename)
+	return df
+
+def mung_na(df,colname,colval_new):
+	df[colname].fillna(colval_new,, inplace=True)
+	return df
+	
+train_df=read_CSV('./files/train.csv')
+
+train_df=mung_na(train_df, 'Age','30.0')
+
+
+test_df=read_CSV('./files/test.csv')
+
+test_df=mung_na(test_df, 'Age','30.0')
+test_df=mung_na(test_df, 'Fare','35.6')
+
+classifier = train(train_df)
+tr = test(classifier,test_df)
 print tr
 ## TODO : make this into a nice class
