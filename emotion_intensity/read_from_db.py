7f1578d6257fde213ceaa533fb32f7bf38529802
rupsaijna <rupsajina@gmail.com>
2017-03-17 18:36:03 +0530

    adding ark for tweet parsing see readme inside. also twokenize.

--- emotion_intensity/read_from_db.py
+++ emotion_intensity/read_from_db.py
@@ -4,7 +4,18 @@ import sqlite3
 conn = sqlite3.connect('emo_int.db')
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM tweets WHERE rating > 0.6 AND rating < 0.7" )
+cur.execute("SELECT * FROM tweets WHERE rating > 0.7 AND rating < 0.8 limit 5" )
+ 
+rows = cur.fetchall()
+ 
+for row in rows:
+    print(row)
+
+
+
+print("\n\n")
+
+cur.execute("SELECT * FROM tweets WHERE rating > 0.6 AND rating < 0.7 limit 5" )
  
 rows = cur.fetchall()
  
---
+++ visual/display.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import mpld3
+
+# Scatter points
+fig, ax = plt.subplots()
+np.random.seed(0)
+x, y = np.random.normal(size=(2, 200))
+color, size = np.random.random((2, 200))
+
+ax.scatter(x, y, c=color, s=500 * size, alpha=0.3)
+ax.grid(color='lightgray', alpha=0.7)
+
+
+mpld3.save_html(fig,'/home/rupsa/Desktop/fig.html')
